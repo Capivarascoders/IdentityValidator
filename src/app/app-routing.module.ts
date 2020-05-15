90f0aaef0e4784249cf06cefb0bb993de8b586e7
YasmimVieira <yasmim@4cadia.com>
2020-05-15 17:25:08 -0300

    Change layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/pages/home/home.component';
-import { MyWalletComponent } from '../app/pages/my-wallet/my-wallet.component';
-import { ExtractComponent } from '../app/pages/extract/extract.component';
 import { RequestLootComponent } from '../app/pages/request-loot/request-loot.component';
 import { ProfileComponent } from '../app/components/profile/profile.component';
-import { NotificationComponent } from '../app/pages/notification/notification.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  { path: 'my-wallet', component: MyWalletComponent},
-  { path: 'extract', component: ExtractComponent},
   { path: 'request-loot', component: RequestLootComponent},
   { path: 'profile', component: ProfileComponent},
-  { path: 'notification', component: NotificationComponent}
 
 ];
 
